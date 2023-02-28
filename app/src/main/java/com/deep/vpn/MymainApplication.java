29978a7f759c5fec4c218d2cb541902e2922b2af
deep-gajera <gajeradeep.deep@gmail.com>
2023-02-28 10:30:00 +0530

    push code

---
+++ app/src/main/java/com/deep/vpn/MymainApplication.java
@@ -0,0 +1,12 @@
+package com.deep.vpn;
+
+import com.live.deeplib.vpnmodule.MyApplication;
+
+public class MymainApplication extends MyApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
